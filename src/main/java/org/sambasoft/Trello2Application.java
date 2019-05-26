bcb7e647a65d93d3fbdd3a40c48c62db4db69038
NasipCOM <salmahunov@mail.ru>
2019-05-26 10:40:57 +0600

    Solved problems with Decoration

--- src/main/java/org/sambasoft/Trello2Application.java
+++ src/main/java/org/sambasoft/Trello2Application.java
@@ -2,6 +2,7 @@ package org.sambasoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @SpringBootApplication
 public class Trello2Application {
@@ -9,5 +10,4 @@ public class Trello2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Trello2Application.class, args);
 	}
-
 }
